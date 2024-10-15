e236b08e136387237c326e3030c5329389313aff
srinu <tsrinivasulu1098@gmail.com>
2024-10-15 06:42:36 +0000

    pendingitems

--- src/components/BlogItem/index.js
+++ src/components/BlogItem/index.js
@@ -1,1 +1,23 @@
-// Write your JS code here
+import './index.css'
+
+const BlogItem = props => {
+  const {value} = props
+  const {id, title, imageUrl, avatarUrl, author, topic} = value
+
+  return (
+    <li className="li">
+      <div className="main-flex">
+        <img src={imageUrl} alt={`image${id}`} className="image" />
+        <div className="bottom-flex">
+          <h1 className="heading">{title}</h1>
+          <p className="para">{topic}</p>
+          <div className="author-flex">
+            <img src={avatarUrl} alt={`avatar${id}`} className="avatar" />
+            <p className="author-name">{author}</p>
+          </div>
+        </div>
+      </div>
+    </li>
+  )
+}
+export default BlogItem
--- src/components/BlogList/index.js
+++ src/components/BlogList/index.js
@@ -1,1 +1,38 @@
-// Write your JS code here
+import {Component} from 'react'
+import './index.css'
+import BlogItem from '../BlogItem'
+
+class BlogList extends Component {
+  state = {BlogLists: []}
+
+  componentDidMount = () => {
+    this.getListOFBlogs()
+  }
+
+  getListOFBlogs = async () => {
+    const response = await fetch(`https://apis.ccbp.in/blogs`)
+    const data = await response.json()
+
+    const formatedData = data.map(eachItem => ({
+      id: eachItem.id,
+      title: eachItem.title,
+      imageUrl: eachItem.image_url,
+      avatarUrl: eachItem.avatar_url,
+      author: eachItem.author,
+      topic: eachItem.topic,
+    }))
+    this.setState({BlogLists: formatedData})
+  }
+
+  render() {
+    const {BlogLists} = this.state
+    return (
+      <ul>
+        {BlogLists.map(eachItem => (
+          <BlogItem key={eachItem.id} value={eachItem} />
+        ))}
+      </ul>
+    )
+  }
+}
+export default BlogList
